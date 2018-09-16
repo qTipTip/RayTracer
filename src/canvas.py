4bee827d2b256265dfdf5fd0dbd66fc84dd09243
Ivar Stangeby <istangeby@gmail.com>
2018-09-16 16:33:16 +0200

    To ppm working.

--- src/canvas.py
+++ src/canvas.py
@@ -9,15 +9,28 @@ class Canvas(object):
         self.pixels = [Color(0, 0, 0) for i in range(w * h)]
 
     def write_pixel(self, x, y, color):
-        self.pixels[x * self.width + y] = color
+        self.pixels[y * self.height + x] = color
 
     def __getitem__(self, ij):
         i, j = ij
-        return self.pixels[i * self.width + j]
+        return self.pixels[j * self.height + i]
 
     def to_ppm(self, filename='canvas.ppm'):
-
+        scale = 255
+        header = 'P3\n {} {}\n {}\n'.format(self.width, self.height, scale)
         with open(filename, 'w') as outfile:
-            outfile.write('P3 \n')
-            outfile.write('{} {}\n'.format(self.width, self.height))
-            outfile.write('255')
+            outfile.write(header)
+            for j in range(self.height):
+                for i in range(self.width):
+                    c = self[i, j]
+                    r, g, b = self._format_for_ppm(c, scale)
+                    outfile.write('{} {} {} '.format(r, g, b))
+                outfile.write('\n ')
+
+    def _format_for_ppm(self, c, scale):
+        r, g, b = c.r, c.g, c.b
+        r = max(min(scale, round(scale*r)), 0)
+        g = max(min(scale, round(scale*g)), 0)
+        b = max(min(scale, round(scale*b)), 0)
+
+        return r, g, b
--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -15,9 +15,9 @@ class TestCanvas(TestCase):
     def test_canvas_write_pixel(self):
         c = Canvas(32, 32)
         col = Color(12, 3, 5)
-        c.write_pixel(5, 5, col)
+        c.write_pixel(2, 3, col)
 
-        assert c[5, 5] == col
+        assert c[2, 3] == col
 
     def test_canvas_get_pixel_outofbounds(self):
         c = Canvas(10, 10)
@@ -34,5 +34,32 @@ class TestCanvas(TestCase):
             assert '255' in infile.readline()
         try:
             os.remove(outfile)
-        except:
+        except FileNotFoundError:
+            pass
+
+    def test_canvas_to_ppm_body(self):
+        c = Canvas(5, 3)
+        c1 = Color(1.5, 0, 0)
+        c2 = Color(0, 0.5, 0)
+        c3 = Color(-0.5, 0, 1)
+
+        c.write_pixel(0, 0, c1)
+        c.write_pixel(2, 1, c2)
+        c.write_pixel(4, 2, c3)
+
+        outfile = 'test_canvas_to_ppm_body.ppm'
+        c.to_ppm(outfile)
+
+        with open(outfile, 'r') as infile:
+            [infile.readline() for i in range(3)]  # skip header
+            body = list(map(str.strip, infile.readlines()))
+            l1 = "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+            l2 = "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"
+            l3 = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"
+            assert body[0] == l1
+            assert body[1] == l2
+            assert body[2] == l3
+        try:
+            os.remove(outfile)
+        except FileNotFoundError:
             pass
