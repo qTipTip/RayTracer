a31a0cebe2ad12aec026d57a64385ecf7ed6cb17
Ivar Stangeby <istangeby@gmail.com>
2018-09-16 18:38:29 +0200

    Multiplication and equality working.

--- src/matrix.py
+++ src/matrix.py
@@ -11,3 +11,28 @@ class Matrix(object):
     def __getitem__(self, item):
         i, j = item
         return self.elements[i][j]
+
+    def __eq__(self, other):
+
+        if self.n == other.n and self.m == other.m:
+            for i in range(self.n):
+                for j in range(self.m):
+                    if self[i, j] != other[i, j]:
+                        return False
+            else:
+                return True
+        else:
+            return False
+
+    def __matmul__(self, other):
+        if self.m != other.n:
+            raise ValueError('Matrix dimensions not compatible')
+
+        elements = [
+            [
+                sum([self[i, k] * other[k, j] for k in range(self.n)])
+                for j in range(self.n)
+            ]
+            for i in range(self.m)
+        ]
+        return Matrix(elements)
