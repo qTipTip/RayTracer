0154ff8e68cece1992c2893f6379e15d94adab6c
Ivar Stangeby <istangeby@gmail.com>
2018-09-17 00:27:52 +0200

    Added Hit-property.

--- src/intersection.py
+++ src/intersection.py
@@ -7,7 +7,7 @@ class Intersection(object):
 
 class Intersections(object):
     def __init__(self, *args):
-        self.objects = args
+        self.objects = list(args)
 
     def __len__(self):
         return len(self.objects)
@@ -17,3 +17,10 @@ class Intersections(object):
 
     def __setitem__(self, key, value):
         self.objects[key] = value
+
+    @property
+    def hit(self):
+        candidates = [i for i in self.objects if i.t >= 0]
+        if len(candidates) == 0:
+            return None
+        return min(candidates, key=lambda i: i.t)
--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -19,3 +19,45 @@ def test_intserction_aggregation():
     assert len(x) == 2
     assert x[0] == i1
     assert x[1] == i2
+
+
+def test_hit_positive():
+    s = Sphere()
+    i1 = Intersection(s, 1)
+    i2 = Intersection(s, 2)
+    x = Intersections(i1, i2)
+
+    h = x.hit
+    assert h == i1
+
+
+def test_hit_one_negative():
+    s = Sphere()
+    i1 = Intersection(s, -1)
+    i2 = Intersection(s, 2)
+    x = Intersections(i1, i2)
+
+    h = x.hit
+    assert h == i2
+
+
+def test_hit_all_negative():
+    s = Sphere()
+    i1 = Intersection(s, -1)
+    i2 = Intersection(s, -2)
+    x = Intersections(i1, i2)
+
+    h = x.hit
+    assert h is None
+
+
+def test_hit_always_lowest_non_negative():
+    s = Sphere()
+    i1 = Intersection(s, -1)
+    i2 = Intersection(s, 2)
+    i3 = Intersection(s, 6)
+    i4 = Intersection(s, 3)
+    x = Intersections(i1, i2, i3, i4)
+
+    h = x.hit
+    assert h is i2
