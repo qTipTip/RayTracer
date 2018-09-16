4c1bf4ddd6dfb7812c39cb8bfa2aa5d7bd61bbd1
Ivar Stangeby <istangeby@gmail.com>
2018-09-16 20:02:10 +0200

    Added scaling.

--- src/transformations.py
+++ src/transformations.py
@@ -7,3 +7,11 @@ def translation(x, y, z):
     i[1, 3] = y
     i[2, 3] = z
     return i
+
+
+def scaling(x, y, z):
+    i = IdentityMatrix(4)
+    i[0, 0] = x
+    i[1, 1] = y
+    i[2, 2] = z
+    return i
---
+++ tests/test_transformations.py
@@ -0,0 +1,45 @@
+from src.transformations import translation, scaling
+from src.tupl import Point, Vector
+
+
+def test_translation():
+    t = translation(5, -3, 2)
+    p = Point(-3, 4, 5)
+
+    assert t * p == Point(2, 1, 7)
+
+
+def test_translation_inverse():
+    t = translation(5, -3, 2)
+    inv = t.inv
+    p = Point(-3, 4, 5)
+
+    assert inv * p == Point(-8, 7, 3)
+
+
+def test_translation_vector_invariance():
+    t = translation(5, -3, 2)
+    v = Vector(-3, 4, 5)
+
+    assert t * v == v
+
+
+def test_scaling_point():
+    t = scaling(2, 3, 4)
+    p = Point(-4, 6, 8)
+    assert t * p == Point(-8, 18, 32)
+
+
+def test_scaling_vector():
+    t = scaling(2, 3, 4)
+    p = Vector(-4, 6, 8)
+
+    assert t * p == Vector(-8, 18, 32)
+
+
+def test_scaling_inverse():
+    t = scaling(2, 3, 4)
+    inv = t.inv
+    v = Vector(-4, 6, 8)
+
+    assert inv * v == Vector(-2, 2, 2)
